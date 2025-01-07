32b9a3ebc03a88739e650b9adf4238e7e7476d8b
yt-bads <aryaxeo123@gmail.com>
2025-01-07 12:51:00 +0700

    aisyah-portofolio

---
+++ main.js
@@ -0,0 +1,29 @@
+// === NAVIGATION BAR FUNCTION === //
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+// === ADD SHADOW ON NAVIGATION BAR SCROLLING === //
+window.onscroll = function() {HeaderShadow()};
+
+function HeaderShadow() {
+    const navHeader = document.getElementById("header");
+    
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, .1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+
+    } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
